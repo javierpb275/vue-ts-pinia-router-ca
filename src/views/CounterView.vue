3924ac89e1ce80038ecb5b65e8c7bd7c544dd965
Javier Pardinas <javier.pardinas@colex.local>
2022-03-17 11:03:00 +0100

    accessing state of counterStore

--- src/views/CounterView.vue
+++ src/views/CounterView.vue
@@ -1,3 +1,12 @@
+<script lang="ts" setup>
+import {useCounterStore} from '../stores/counter';
+
+const counterStore = useCounterStore();
+</script>
+
 <template>
-    <h1>Counter View</h1>
+    <h1>
+    Counter View: {{counterStore.counter}}
+    </h1>
+    <h3>Number of changes: {{counterStore.numberOfChanges}}</h3>
 </template>
