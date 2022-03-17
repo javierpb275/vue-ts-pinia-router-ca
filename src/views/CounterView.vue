df1d7c0119518454ce5eafa0393906f6fa5dc1f0
Javier Pardinas <javier.pardinas@colex.local>
2022-03-17 11:12:40 +0100

    using action incrementBy() from the counter store in CounterView

--- src/views/CounterView.vue
+++ src/views/CounterView.vue
@@ -2,11 +2,17 @@
 import {useCounterStore} from '../stores/counter';
 
 const counterStore = useCounterStore();
+
+const increaseBy = (value: number) => {
+    counterStore.incrementBy(value);
+}
 </script>
 
 <template>
     <h1>
     Counter View: {{counterStore.counter}}
     </h1>
-    <h3>Number of changes: {{counterStore.numberOfChanges}}</h3>
+    <button @click="increaseBy(1)">+1</button>
+    <button @click="increaseBy(2)">+2</button>
+    <button @click="increaseBy(5)">+5</button>
 </template>
