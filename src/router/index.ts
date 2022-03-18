bf491bb92f9686957b0bec22802eba989f35c62a
Javier Pardinas <javier.pardinas@colex.local>
2022-03-18 09:43:38 +0100

    prepared routes in router

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import HomeView from '../views/HomeView.vue';
 import CounterView from '../views/CounterView.vue';
 import CartView from '../views/CartView.vue';
+//import CatsView from '../views/CatsView.vue';
 
 const routes: RouteRecordRaw[] = [
     {
@@ -18,7 +19,28 @@ const routes: RouteRecordRaw[] = [
         path: '/cart',
         name: 'Cart',
         component: CartView
-    }
+    },
+/*     {
+        path: '/cats',
+        name: 'Cats',
+        component: CatsView
+    }, 
+    {
+        path: '/cats/fact/:id',
+        name: 'Fact',
+        component: () => import('../views/Fact.vue'),
+        beforeEnter: (to, _, next) => {}
+    },
+    {
+        path: '/cats/facts/',
+        name: 'FactList',
+        component: () => import('../views/FactList.vue'),
+    },
+    {
+        path: '/:catchAll(.*)',
+        name: 'PageNotFound',
+        component: () => import('../views/PageNotFound.vue')
+    }*/
 ];
 
 const router = createRouter({
